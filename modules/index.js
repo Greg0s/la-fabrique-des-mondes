e503f1c8a7e2afaef788f9b1a7f6babfe3f0cbb3
Greg0s <gregoire.tinnes@outlook.fr>
2023-10-27 12:02:47 +0200

    Add: modules export index

---
+++ modules/index.js
@@ -0,0 +1,6 @@
+export { default as Boids } from "./boids.js";
+export { default as Sponge } from "./sponge.js";
+export { default as TSP } from "./salesman.js";
+export { default as FitnessLandscape } from "./fitness-landscape.js";
+export { default as SierpinskiTriangle } from "./sierpinski-triangle.js";
+export { default as StrangeAttractor } from "./strange-attractors.js";
