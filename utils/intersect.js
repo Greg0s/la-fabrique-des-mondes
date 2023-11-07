f05c25c76e14276e353ec37bf40354b211a4dcd8
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-07 23:35:17 +0100

    Fix: clean intersect utils

--- utils/intersect.js
+++ utils/intersect.js
@@ -1,20 +1,26 @@
 const intersectionHandler = (scene, control, raycaster) => {
   let INTERSECTED;
   const intersects = raycaster.intersectObjects(scene.children, false);
+  // console.log("tableau", scene.children);
 
   if (intersects.length > 0) {
     if (INTERSECTED != intersects[0].object) {
-      if (INTERSECTED)
-        INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+      intersects.forEach((intersect) => {
+        const intTemp = intersect.object;
+        control.attach(intTemp);
+      });
+      // if (INTERSECTED)
+      // INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+      // console.log(INTERSECTED);
       INTERSECTED = intersects[0].object;
-      INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-      INTERSECTED.material.emissive.setHex(0xff0000);
+      // INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+      // INTERSECTED.material.emissive.setHex(0xff0000);
 
-      control.attach(INTERSECTED);
+      // control.attach(INTERSECTED);
     }
   } else {
-    if (INTERSECTED)
-      INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
+    // if (INTERSECTED)
+    //   INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
     control.detach();
     INTERSECTED = null;
   }
