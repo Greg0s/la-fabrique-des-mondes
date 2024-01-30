bd5505eb913e77bfcf26e8073b629704b5884771
Greg0s <gregoire.tinnes@outlook.fr>
2024-01-30 16:30:46 +0100

    Remove: old ui

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,5 @@
 export { default as tools } from "./tools.js";
 export { default as intersectionHandler } from "./intersect.js";
-export { default as UI } from "./ui.js";
 export { default as Hitbox } from "./hitboxClass.js";
 export { saveScreenshot as saveScreenshot } from "./screenshot.js";
 export { firstPopUp as firstPopUp } from "./story.js";
