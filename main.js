559ef3cb51b368a7819a309c6aa182de81a1de8f
Greg0s <gregoire.tinnes@outlook.fr>
2024-10-17 11:01:08 +0200

    Update background link

--- main.js
+++ main.js
@@ -159,7 +159,7 @@ function init() {
   //Fond image
 
   const loader = new THREE.TextureLoader();
-  const texture = loader.load("./img/fond.png", () => {
+  const texture = loader.load("https://i.imgur.com/J3FY6xN.jpeg", () => {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     texture.colorSpace = THREE.SRGBColorSpace;
     mainScene.background = texture;
